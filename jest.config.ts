cddf9cd14439dcd4e759a7dc44308d4108f407fe
Chris Hayes <christopherhayes@gmail.com>
2024-12-23 18:57:51 -0500

    more jest config

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+module.exports = {
+  moduleDirectories: ['node_modules', 'release/app/node_modules', 'src'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/.erb/mocks/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  setupFiles: ['./.erb/scripts/check-build-exists.ts'],
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost/',
+  },
+  testPathIgnorePatterns: ['release/app/dist', '.erb/dll'],
+  transform: {
+    '\\.[jt]sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    // Add this to handle ES modules in node_modules
+    'node_modules/(?!(react-markdown|devlop|vfile|unist-.*|unified|bail|is-plain-obj|trough|remark-.*|mdast-util-.*|micromark.*|decode-named-character-reference|character-entities|property-information|hast-util-whitespace|space-separated-tokens|comma-separated-tokens|/@radix-ui)/)',
+  ],
+}; 
